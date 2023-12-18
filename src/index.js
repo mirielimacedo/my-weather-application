7921d223d5dcde195e65be29489f394122cd0ad2
mirielimacedo <mirielimacedo@gmail.com>
2023-12-18 16:11:27 +0800

    update seachCity function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+function searchcity(city){
+let apiKey = "f9b4d69b4d22303f3t650a59od86ede0";
+let urlApi = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`; 
+}
+
+
+
+
 function handSearchSubmit(event){
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchInput(searchInput.value);
 }
 
 
