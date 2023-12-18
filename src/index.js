6e61697f013ced7e4de2fff7db8e90a39c9ac228
mirielimacedo <mirielimacedo@gmail.com>
2023-12-18 16:18:04 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function refreshWeather(response){
 let temperatureElement = document.querySelector("#temperature");
-temperatureElement.innerHTML = response.data.current.temperature;
+temperatureElement.innerHTML = response.data.temperatue.currente;
 }
 
 
