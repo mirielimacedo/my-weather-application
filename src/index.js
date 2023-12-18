0e0ea97293a4b2d16302d055a8fd244419c8a2ed
mirielimacedo <mirielimacedo@gmail.com>
2023-12-18 16:42:35 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,16 +6,23 @@ console.log(response.data.temperature.current);
 }
 
 
-function searchcity(city){
+function searchCity(city){
 let apiKey = "f9b4d69b4d22303f3t650a59od86ede0";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`; 
 axios.get(apiUrl).then(refreshWeather);
 }
 
 
+function handSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value); // Esta é a chamada correta para a função searchcity
+}
 
 
-function handSearchSubmit(event){
+/*function handSearchSubmit(event){
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
@@ -25,7 +32,7 @@ function handSearchSubmit(event){
 
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit",handSearchSubmit);
+searchFormElement.addEventListener("submit",handSearchSubmit);*/
 
 
 
