357aae690ab036ca7b98cac6adcc5c9181ad6b56
mirielimacedo <mirielimacedo@gmail.com>
2023-12-18 14:41:05 +0800

    function handSearchSubmit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-alert("hello word");
+function handsearchSubmit(event){
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  console.log(searchInput.value);
+}
+
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit",handsearchSubmit);
 
 
 
